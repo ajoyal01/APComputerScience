4b7ef7762ca5b06be3e7395cb503f16d6ffb9968
ajjoyal01 <ajjoyal01@users.noreply.github.com>
2017-09-20 22:18:00 -0400

    initial commit

---
+++ ArithmeticPractice/src/ArithmeticPractice.java
@@ -0,0 +1,11 @@
+
+public class ArithmeticPractice {
+
+	public static void main(String[] args) {
+		System.out.println(9 % 5);
+		System.out.println(9.3 % 5.1);
+		System.out.println(9.3 % 5);
+		System.out.println(9 % 5.1);
+	}
+
+}
